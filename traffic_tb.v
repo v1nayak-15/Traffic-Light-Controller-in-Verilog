439d30709a535a2ca6bbdbf6257265a11852841e
v1nayak-15 <vinayak.vinod.2381@gmail.com>
2025-09-16 22:45:49 +0530

    traffic_tb.v

--- traffic_tb.v
+++ traffic_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/02/2024 02:16:19 PM
-// Design Name: 
-// Module Name: traffic_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module traffic_tb;
 
